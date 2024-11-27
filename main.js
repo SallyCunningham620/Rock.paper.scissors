fe66ebcf0f11c464852de8caa4b17f51861d9d59
Sally <143855164+SallyCunningham620@users.noreply.github.com>
2024-11-27 06:40:43 -0600

    Add HTML and JS for game

--- main.js
+++ main.js
@@ -1,20 +1,58 @@
-console.log("Hello World!");
+let playerScore = 0;
+let computerScore = 0;
+const roundsToWin = 5;
+
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerScore");
+const computerDisplaye = document.getElementById("computerScore");
+const resultDisplay = document.getElementById("result");
+
 
 function getComputerChoice() {
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min +1) + min);
-    }
-    let randomComputerChoice = getRandomInt(1, 3);
-    function computerChoice() {
-        if (randomComputerChoice == 1) {
-            return "rock";
-        } else if (randomComputerChoice == 2) {
-            return "paper";
-        } else if (randomComputerChoice == 3) {
-            return "scissors"
-        } else "An error occured"
-    }
-    return computerChoice();
+    const choice = choices[Math.floor(Math.random() * choices.length)];
+    return choice;
+}
+function playGame(playerChoice){
+    const computerChoice = getComputerChoice();
+    let result = "";
+    if (playerChoice === computerChoice) {
+        result = "IT'S A TIE"
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+      ) {
+        result = "You win!";
+        playerScore++;
+      } else {
+        result = "You lose!";
+        computerScore++;
+      }
+
+    document.getElementById("playerScore").textContent = playerScore;
+    document.getElementById("computerScore").textContent = computerScore;
+    document.getElementById("result").textContent = `You chose ${playerChoice}, computer chose ${computerChoice}. ${result}`;
+
+
+    checkWinner();
 }
 
-console.log(getComputerChoice());
+
+
+function checkWinner() {
+    if (playerScore === roundsToWin) {
+        alert("Player wins the game!");
+        resetGame();
+    } else if (computerScore === roundsToWin) {
+        alert("Computer wins the game!");
+        resetGame();
+    }
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    document.getElementById("playerScore").textContent = 0;
+    document.getElementById("computerScore").textContent = 0;
+    document.getElementById("result").textContent = "";
+}
